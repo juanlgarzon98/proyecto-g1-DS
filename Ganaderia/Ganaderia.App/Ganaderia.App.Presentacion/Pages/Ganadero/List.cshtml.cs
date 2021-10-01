d8af04075c5e293418998a182f70663be3a4b869
juanlgarzon <juan98.lozanog@gmail.com>
2021-09-30 22:02:02 -0500

    paginas registrar login listaGanadero

--- Ganaderia/Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ganadero/List.cshtml.cs
+++ Ganaderia/Ganaderia.App/Ganaderia.App.Presentacion/Pages/Ganadero/List.cshtml.cs
@@ -4,13 +4,27 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Ganaderia.App.Dominio;
 
 namespace Ganaderia.App.Presentacion.Pages
 {
     public class ListModel : PageModel
     {
+        public List<Ganadero> Ganaderos { get; set; }
         public void OnGet()
         {
+            var ganadero = new Ganadero
+            {
+                Nombres = "Sergio",
+                Apellidos = "Moreno",
+                NumeroTelefono = "3148596562",
+                Correo = "sergiomintic@mintic.edu.co",
+                Contrasena = "123456",
+                Latitud = 4554,
+                Longitud = 5454
+            };
+        Ganaderos=new List<Ganadero>();
+        Ganaderos.Add(ganadero);
         }
     }
 }
